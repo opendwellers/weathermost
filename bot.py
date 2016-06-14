42bfbbd4968a060dc2dae9109938c7cfbdb3814f
Patrick-Jeffrey Pollo Guilbert <patrick.pollo.guilbert@ericsson.com>
2016-06-14 16:56:57 -0400

    Several changes
    Finish payload formatting
    Add function comments
    Add .gitignore to not commit the token by accident

--- bot.py
+++ bot.py
@@ -7,40 +7,40 @@ import json
 
 
 app = Flask(__name__)
-url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Montreal&APPID={key}".format
+url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Montreal&APPID={key}&cnt=5".format
 mattermost_url = ""
 
 def init():
-   global url
-   global mattermost_url
-   Config = configparser.ConfigParser()
-   Config.read("configuration/config")
-   Config.sections()
+    """Initializes variables"""
+    global url
+    global mattermost_url
+    Config = configparser.ConfigParser()
+    Config.read("configuration/config")
+    Config.sections()
 
-   config_api_key = Config.get('OpenWeather', 'api_key')
-   url = url(key=config_api_key)
+    config_api_key = Config.get('OpenWeather', 'api_key')
+    url = url(key=config_api_key)
 
-   mattermost_url = Config.get('Mattermost', 'url')
-
-
-class Weather(object):
-    def __init__(self, j):
-        self.__dict__ = json.loads(j)
+    mattermost_url = Config.get('Mattermost', 'url')
 
 @app.route('/weather', methods=['POST'])
 def get_weather():
+    """Bot receives a /slash command
+    TODO add option to choose city with /weather toronto
+    """
     r = requests.get(url)
     post_mattermost(r.json())
     return
 
 def get_embedded_icon_url(icon_code, desc):
-    return '![desc](http://openweathermap.org/img/w/{code}.png) "{desc}"'.format(code=icon_code, desc=desc)
+    """Returns a formatted markdown line to show the weather icon"""
+    return '![desc](http://openweathermap.org/img/w/{code}.png "{desc}")'.format(code=icon_code, desc=desc)
 
 def get_day_weather_line(day):
     """Return a markdown formatted line for a weather day"""
-    day_weekday = datetime.fromtimestamp(day['dt']/1000).strftime("%A")
-    day_month = datetime.fromtimestamp(day['dt']/1000).strftime("%b")
-    day_day = datetime.fromtimestamp(day['dt']/1000).strftime("%d")
+    day_weekday = datetime.fromtimestamp(day['dt']).strftime("%A")
+    day_month = datetime.fromtimestamp(day['dt']).strftime("%b")
+    day_day = datetime.fromtimestamp(day['dt']).strftime("%d")
     day_info_date = """{weekday}, {month}. {day_number}""".ljust(25).format(weekday=day_weekday, month=day_month, day_number=day_day)
     #print(day_info_date)
     day_desc = day['weather'][0]['description']
@@ -51,29 +51,25 @@ def get_day_weather_line(day):
     day_temp_low = int(day['temp']['min'] - 273.15)
     #print(day_temp_low)
     day_icon = get_embedded_icon_url(day['weather'][0]['icon'], day_desc)
-    return "| {day_info_date_param} | {desc_param} | {day_temp_high_param} °C | {day_temp_low_param} °C ".format(day_info_date_param=day_info_date, desc_param=day_desc, day_temp_high_param=day_temp_high, day_temp_low_param=day_temp_low)
+    return "| {day_info_date_param} | {desc_param}  {day_icon_param} | {day_temp_high_param} °C | {day_temp_low_param} °C  |\n".format(day_info_date_param=day_info_date, desc_param=day_desc, day_icon_param=day_icon, day_temp_high_param=day_temp_high, day_temp_low_param=day_temp_low)
 
 
 def post_mattermost(data):
+    """Post formatted data to mattermost instance"""
     days = []
+    payload_text = """
+---
+### Weather in Montreal, Quebec, for the next few days
+
+| Day | Description | High | Low |
+|:---------------------------|:------------------------------------|:--------|:--------|\n"""
     for day in data['list']:
-        print(get_day_weather_line(day))
+        #print(get_day_weather_line(day))
+        payload_text += get_day_weather_line(day)
+    payload_text += "---"
+    print(payload_text)
 
-    payload = {"response_type": "in_channel", "text": """
-               ---
-               #### Weather in Montreal, Quebec for the next few days
-
-               | Day                 | Description                      | High   | Low    |
-               |:--------------------|:---------------------------------|:-------|:-------|
-               | Monday, Feb. 15     | Cloudy with a chance of flurries | 3 °C   | -12 °C |
-               | Tuesday, Feb. 16    | Sunny                            | 4 °C   | -8 °C  |
-               | Wednesday, Feb. 17  | Partly cloudly                   | 4 °C   | -14 °C |
-               | Thursday, Feb. 18   | Cloudy with a chance of rain     | 2 °C   | -13 °C |
-               | Friday, Feb. 19     | Overcast                         | 5 °C   | -7 °C  |
-               | Saturday, Feb. 20   | Sunny with cloudy patches        | 7 °C   | -4 °C  |
-               | Sunday, Feb. 21     | Partly cloudy                    | 6 °C   | -9 °C  |
-               ---
-               """}
+    payload = {"response_type": "in_channel", "text": payload_text}
 
     requests.post(mattermost_url, data=json.dumps(payload), verify=False)
 
